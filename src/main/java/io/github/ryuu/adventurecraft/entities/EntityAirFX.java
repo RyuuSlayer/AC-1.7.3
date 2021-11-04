88a8d2593cc364962f7c06cd7ae2e6493ef5336f
letsp <letsphil12@gmail.com>
2021-11-04 11:11:59 +0100

    ScriptEntity / EntityAirFX update

--- src/main/java/io/github/ryuu/adventurecraft/entities/EntityAirFX.java
+++ src/main/java/io/github/ryuu/adventurecraft/entities/EntityAirFX.java
@@ -6,11 +6,11 @@ import net.minecraft.level.Level;
 public class EntityAirFX extends Particle {
     public EntityAirFX(Level world, double d, double d1, double d2) {
         super(world, d, d1, d2, 0.0D, 0.0D, 0.0D);
-        this.b = 0;
-        this.i = this.j = this.k = 1.0F;
-        this.h = 0.0F;
-        this.g /= 2.0F;
-        this.aP = this.aQ = this.aR = 0.0D;
+        this.field_2635 = 0;
+        this.field_2642 = this.field_2643 = this.field_2644 = 1.0F;
+        this.field_2641 = 0.0F;
+        this.field_2640 /= 2.0F;
+        this.velocityX = this.velocityY = this.velocityZ = 0.0D;
     }
 
     public int c_() {
